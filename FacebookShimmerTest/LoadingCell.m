e0561fa44a84c6d6857686d1465d7397564c7977
Brandon T <brandon@plasticmobile.com>
2016-10-29 15:41:59 -0400

    Added TestMacro

--- FacebookShimmerTest/LoadingCell.m
+++ FacebookShimmerTest/LoadingCell.m
@@ -11,6 +11,8 @@
 #import <Shimmer/FBShimmeringView.h>
 
 
+//#define kDisableFBShimmering
+
 @interface LoadingCell()
 @property (nonnull, nonatomic, strong) UIView *topView;
 @property (nonnull, nonatomic, strong) BottomView *bottomView;
@@ -45,7 +47,12 @@
 
 - (void)doLayout {
     [self.contentView addSubview:self.topView];
+    
+    #if !defined(kDisableFBShimmering)
     [self.contentView addSubview:self.shimmerView];
+    #else
+    [self.contentView addSubview:self.bottomView];
+    #endif
     
     
     [self.topView.leftAnchor constraintEqualToAnchor:self.contentView.leftAnchor].active = YES;
@@ -53,13 +60,25 @@
     [self.topView.topAnchor constraintEqualToAnchor:self.contentView.topAnchor].active = YES;
     [self.topView.heightAnchor constraintEqualToConstant:175.0].active = YES;
     
+    #if !defined(kDisableFBShimmering)
     [self.shimmerView.leftAnchor constraintEqualToAnchor:self.contentView.leftAnchor].active = YES;
     [self.shimmerView.rightAnchor constraintEqualToAnchor:self.contentView.rightAnchor].active = YES;
     [self.shimmerView.topAnchor constraintEqualToAnchor:self.topView.bottomAnchor].active = YES;
     [self.shimmerView.bottomAnchor constraintEqualToAnchor:self.contentView.bottomAnchor].active = YES;
+    #else
+    [self.bottomView.leftAnchor constraintEqualToAnchor:self.contentView.leftAnchor].active = YES;
+    [self.bottomView.rightAnchor constraintEqualToAnchor:self.contentView.rightAnchor].active = YES;
+    [self.bottomView.topAnchor constraintEqualToAnchor:self.topView.bottomAnchor].active = YES;
+    [self.bottomView.bottomAnchor constraintEqualToAnchor:self.contentView.bottomAnchor].active = YES;
+    #endif
     
     [self.topView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    #if !defined(kDisableFBShimmering)
     [self.shimmerView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    #else
+    [self.bottomView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    #endif
 }
 
 
@@ -72,6 +91,8 @@
 }
 
 - (void)shimmer {
+    #if !defined(kDisableFBShimmering)
     [self.shimmerView setShimmering:YES];
+    #endif
 }
 @end
